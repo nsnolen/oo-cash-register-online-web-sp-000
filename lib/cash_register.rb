1a4f9719a5636a7f87cf93e8d848a6f6c71d8f0f
Sara Nolen <nolensara1@gmail.com>
2021-02-04 02:25:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-  attr_accessor :employee_discount :total
+  attr_accessor :employee_discount, :total
 
   def initialize(employee_discount, total = 0)
     @employee_discount = employee_discount
